684e9d25e0954abe667fd7abc43d528f103624c0
DinakiS <dinaki2017@yandex.ru>
2018-07-07 22:39:31 +0300

    Search in file

--- src/renderer/components/PageContent/ShowRpy.vue
+++ src/renderer/components/PageContent/ShowRpy.vue
@@ -1,6 +1,7 @@
 <template>
   <div v-if="loading" class='loading'></div>
-  <div v-else class="rpy-code">
+  <div v-else class="rpy-code" v-hotkey="keymap">
+    <search class='search' v-if="showSearch"/>
     <pre v-html="code"></pre>
   </div>
 </template>
@@ -10,22 +11,28 @@ import fs from 'fs'
 import Prism from 'prismjs'
 import prismRenpy from '../../libs/prism-renpy'
 
+import Search from './code/SearchInFile.vue'
+
 Prism.languages.renpy = prismRenpy
 
 export default {
   props: ['file'],
+  components: { Search },
   data () {
     return {
       loading: false,
-      code_text: null,
       code: null,
-      searchText: '',
-      _searchTextRegExp: null
+      showSearch: false
     }
   },
   computed: {
     currentFile () {
       return this.$store.state.files.currentFile
+    },
+    keymap () {
+      return {
+        'ctrl+f': this.toggleSearch
+      }
     }
   },
   watch: {
@@ -36,6 +43,9 @@ export default {
     }
   },
   methods: {
+    toggleSearch () {
+      this.showSearch = !this.showSearch
+    },
     readFile () {
       this.loading = true
 
@@ -44,7 +54,7 @@ export default {
 
         this.code_text = data
         if (this.$store.state.config.highlightCode) {
-        this.code = Prism.highlight(data, Prism.languages.renpy, 'renpy')
+          this.code = Prism.highlight(data, Prism.languages.renpy, 'renpy')
         } else {
           this.code = data
         }
@@ -66,6 +76,17 @@ export default {
     font-family: monospace;
     padding: 20px;
     position: relative;
+
+    .search {
+      position: fixed;
+      top: 70px;
+      right: 13px;
+    }
+    pre {
+      span.search-highlight {
+        background-color: rgba(125, 171, 255, 0.4);
+      }
+    }
   }
 </style>
 
